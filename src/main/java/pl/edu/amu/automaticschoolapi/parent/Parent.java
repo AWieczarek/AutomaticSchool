96a55b342ac887fc27d2af792fbf31c57637d2aa
AWieczarek <adamwie12@gmail.com>
2022-02-16 19:00:04 +0100

    Added few field to Parent class

--- src/main/java/pl/edu/amu/automaticschoolapi/parent/Parent.java
+++ src/main/java/pl/edu/amu/automaticschoolapi/parent/Parent.java
@@ -1,4 +1,35 @@
 package pl.edu.amu.automaticschoolapi.parent;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table(name = "parents")
 public class Parent {
+
+    @Id
+    private long id;
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @NotBlank
+    private String surname;
+
+    @NotNull
+    @Pattern(regexp="(^$|[0-9]{9})")
+    private String phoneNumber;
 }
